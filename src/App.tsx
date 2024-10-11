5dbd68b9381befeb6caebead08808f4a4958802a
Faculdade Embu das Artes <90737370+JeanPDR@users.noreply.github.com>
2024-10-11 09:06:41 -0300

    finalizando estrutura base do formulário

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,28 @@
+import { useState } from "react";
 import "./styles/globals.css";
 
+import { useForm } from "react-hook-form";
+
 function App() {
+  const [output, setOutPut] = useState("");
+  const { register, handleSubmit } = useForm();
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  function createUser(data: any) {
+    setOutPut(JSON.stringify(data, null, 2));
+  }
   return (
     <>
-      <main className="h-screen bg-zinc-50 flex items-center justify-center">
-        <form className=" flex flex-col gap-4 w-full max-w-xs">
+      <main className="h-screen bg-zinc-50 flex flex-col gap-10 items-center justify-center">
+        <form
+          onSubmit={handleSubmit(createUser)}
+          className=" flex flex-col gap-4 w-full max-w-xs"
+        >
           <div className=" flex flex-col gap-1">
             <label htmlFor="">E-mail</label>
             <input
               type="email"
-              name="email"
+              {...register("email")}
               className="border border-zinc-200 shadow-sm rounded h-10"
             />
           </div>
@@ -17,7 +30,7 @@ function App() {
             <label htmlFor="">Senha</label>
             <input
               type="password"
-              name="password"
+              {...register("password")}
               className="border border-zinc-200 shadow-sm rounded h-10"
             />
           </div>
@@ -28,6 +41,7 @@ function App() {
             Salvar
           </button>
         </form>
+        <pre>{output}</pre>
       </main>
     </>
   );
