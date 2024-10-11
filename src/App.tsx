358ce5f376d6bb03da8d1411e68f4b92cf4d6eda
Faculdade Embu das Artes <90737370+JeanPDR@users.noreply.github.com>
2024-10-10 22:55:15 -0300

    finalizando primeira etapa do formulário

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,32 @@ import "./styles/globals.css";
 function App() {
   return (
     <>
-      <h1>Hello Word</h1>
+      <main className="h-screen bg-zinc-50 flex items-center justify-center">
+        <form className=" flex flex-col gap-4 w-full max-w-xs">
+          <div className=" flex flex-col gap-1">
+            <label htmlFor="">E-mail</label>
+            <input
+              type="email"
+              name="email"
+              className="border border-zinc-200 shadow-sm rounded h-10"
+            />
+          </div>
+          <div className=" flex flex-col gap-1">
+            <label htmlFor="">Senha</label>
+            <input
+              type="password"
+              name="password"
+              className="border border-zinc-200 shadow-sm rounded h-10"
+            />
+          </div>
+          <button
+            type="submit"
+            className="bg-emerald-500 rounded font-semibold text-white h-10 hover:bg-emerald-600"
+          >
+            Salvar
+          </button>
+        </form>
+      </main>
     </>
   );
 }
